080fd9876d283f9b2bb7fcefca69a186a528aba7
DurgaPrasadSonti <durgaprasadsonti1998@gmail.com>
2025-07-30 23:05:23 +0530

    Update Tomcat Deployment.sh

--- Tomcat Deployment.sh
+++ Tomcat Deployment.sh
@@ -8,7 +8,7 @@ pipeline {
     stages {
         stage('Git Checkout') {
             steps {
-                git branch: 'main', url: 'https://github.com/Ushaprasad143/Petclinic-Jenkins-Project.git'
+                git branch: 'main', url: 'https://github.com/DurgaPrasadSonti/Petclinic-Jenkins-Project.git'
             }
         }
         
